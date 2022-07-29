2043b42ebb11a8badb7b9ffec0de96ee5307fdce
J.Pochet <jpochet@lhermitte.fr>
2022-07-29 12:09:52 +0200

    ajout de la selection des mails de reception pour roby uniquement

--- src/Controller/OldCmdController.php
+++ src/Controller/OldCmdController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use DateTime;
+use App\Form\AddEmailType;
+use App\Entity\Main\MailList;
 use Symfony\Component\Mime\Email;
 use App\Entity\Main\ListCmdTraite;
+use App\Controller\AdminEmailController;
 use App\Repository\Divalto\EntRepository;
 use App\Repository\Main\MailListRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,15 +29,16 @@ class OldCmdController extends AbstractController
     private $repoMail;
     private $mailEnvoi;
     private $mailTreatement;
+    private $adminEmailController;
 
-    public function __construct(MailListRepository $repoMail, ListCmdTraiteRepository $repoNumCmd,MailerInterface $mailer)
+    public function __construct(AdminEmailController $adminEmailController, MailListRepository $repoMail, ListCmdTraiteRepository $repoNumCmd,MailerInterface $mailer)
     {
         $this->repoNumCmd = $repoNumCmd;
         $this->mailer = $mailer;
         $this->repoMail =$repoMail;
         $this->mailEnvoi = $this->repoMail->getEmailEnvoi()['email'];
         $this->mailTreatement = $this->repoMail->getEmailTreatement()['email'];
-
+        $this->adminEmailController = $adminEmailController;
         //parent::__construct();
     }
 
@@ -88,15 +92,50 @@ class OldCmdController extends AbstractController
             $numeros = 1;
         }
 
+        if ($roby == true && $lhermitte == false) {
+            unset($form);
+            $form = $this->createForm(AddEmailType::class);
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()){
+                $find = $this->repoMail->findBy(['email' => $form->getData()['email'], 'page' => $tracking]);
+                if (empty($find) | is_null($find)) {
+                    $mail = new MailList();
+                    $mail->setCreatedAt(new DateTime())
+                        ->setEmail($form->getData()['email'])
+                        ->setPage($tracking);
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($mail);
+                    $em->flush();
+                }else {
+                    $this->addFlash('danger', 'le mail est déjà inscrit pour cette page !');
+                    return $this->redirectToRoute('app_old_cmd');
+                }
+            }
+        }
+        $formRoby = '';
         $oldCmds = $repo->getOldCmds($dos, $numeros);
         $oldCmdsMouv = $repo->getOldCmdsMouv($dos, $numeros);
-        
+        if ($roby == true && $lhermitte == false) {
+            $formRoby = true;
         return $this->render('old_cmd/index.html.twig', [
             'controller_name' => 'OldCmdController',
             'oldCmds' => $oldCmds,
             'oldCmdsMouvs' => $oldCmdsMouv,
-            'title' => 'Vieilles Commandes actives'
+            'title' => 'Vieilles Commandes actives',
+            'listeMails' => $this->repoMail->findBy(['page' => $tracking]),
+            'form' => $form->createView(),
+            'formRoby' => $formRoby,
         ]);
+        }else {
+        return $this->render('old_cmd/index.html.twig', [
+            'controller_name' => 'OldCmdController',
+            'oldCmds' => $oldCmds,
+            'oldCmdsMouvs' => $oldCmdsMouv,
+            'title' => 'Vieilles Commandes actives',
+            'formRoby' => $formRoby,
+        ]);
+        }
+        
     }
 
     /**
@@ -120,16 +159,21 @@ class OldCmdController extends AbstractController
         // envoyer un mail
         $html = $this->renderView('mails/MailDeleteCmd.html.twig', ['lockCmd' => $lockCmd]);
         if ($lockCmd->getDossier() == 1) {
-            $destinataire = $this->mailTreatement;
+            $email = (new Email())
+            ->from($this->mailEnvoi)
+            ->to($this->mailTreatement)
+            ->subject('Message Intranet, merci de supprimer la commande ' . $lockCmd->getNumero() . ' pour le dossier ' . $lockCmd->getDossier())
+            ->html($html);
         }
         if ($lockCmd->getDossier() == 3) {
-            $destinataire = 'ndegorre@roby-fr.com';
+            $treatementMails = $this->repoMail->findBy(['page' => 'app_old_cmd']);
+            $mails = $this->adminEmailController->formateEmailList($treatementMails); 
+            $email = (new Email())
+            ->from($this->mailEnvoi)
+            ->to(...$mails)
+            ->subject('Message Intranet, merci de supprimer la commande ' . $lockCmd->getNumero() . ' pour le dossier ' . $lockCmd->getDossier())
+            ->html($html);
         }
-        $email = (new Email())
-        ->from($this->mailEnvoi)
-        ->to($destinataire)
-        ->subject('Message Intranet, merci de supprimer la commande ' . $lockCmd->getNumero() . ' pour le dossier ' . $lockCmd->getDossier())
-        ->html($html);
         $this->mailer->send($email);
 
         $this->addFlash('message', 'Un mail a été envoyé pour demander la suppresion de cette commande, cette commande n\'apparaitra plus dans la liste, merci !');
