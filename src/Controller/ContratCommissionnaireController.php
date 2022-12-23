045e58791e45bfa5d2d562d06b39c72b28830210
J.Pochet <jpochet@lhermitte.fr>
2022-12-23 10:22:01 +0100

    ajout d'un menu pour visualiser les ventes contrats commissionnaires d'un mois

--- src/Controller/ContratCommissionnaireController.php
+++ src/Controller/ContratCommissionnaireController.php
@@ -2,23 +2,23 @@
 
 namespace App\Controller;
 
-use DateTime;
-use App\Form\AddEmailType;
 use App\Entity\Main\MailList;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\Mime\Address;
+use App\Entity\Main\ProduitsCommissionnaires;
+use App\Form\AddEmailType;
+use App\Form\YearMonthType;
 use App\Repository\Divalto\ArtRepository;
 use App\Repository\Divalto\MouvRepository;
 use App\Repository\Main\MailListRepository;
-use App\Entity\Main\ProduitsCommissionnaires;
-use DoctrineExtensions\Query\Mysql\YearMonth;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\Main\ProduitsCommissionnairesRepository;
+use DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @IsGranted("ROLE_USER")
@@ -32,14 +32,14 @@ class ContratCommissionnaireController extends AbstractController
     private $mailer;
     private $repoMail;
     private $mailEnvoi;
-    
-    public function __construct(ProduitsCommissionnairesRepository $cc, MouvRepository $repoMouv,MailerInterface $mailer, MailListRepository $repoMail)
+
+    public function __construct(ProduitsCommissionnairesRepository $cc, MouvRepository $repoMouv, MailerInterface $mailer, MailListRepository $repoMail)
     {
         $this->mailer = $mailer;
         $this->cc = $cc;
         $this->repoMail = $repoMail;
         $this->repoMouv = $repoMouv;
-        $this->repoMail =$repoMail;
+        $this->repoMail = $repoMail;
         $this->mailEnvoi = $this->repoMail->getEmailEnvoi()['email'];
         //parent::__construct();
     }
@@ -53,24 +53,41 @@ class ContratCommissionnaireController extends AbstractController
         // tracking user page for stats
         $tracking = $request->attributes->get('_route');
         $this->setTracking($tracking);
-
+        $mouvs = "";
+        $art = "";
         unset($form);
         $form = $this->createForm(AddEmailType::class);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $find = $repoMails->findBy(['email' => $form->getData()['email'], 'page' => $tracking]);
             if (empty($find) | is_null($find)) {
                 $mail = new MailList();
                 $mail->setCreatedAt(new DateTime())
-                     ->setEmail($form->getData()['email'])
-                     ->setPage($tracking);
+                    ->setEmail($form->getData()['email'])
+                    ->setPage($tracking);
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($mail);
                 $em->flush();
-            }else {
+            } else {
                 $this->addFlash('danger', 'le mail est déjà inscrit pour cette page !');
                 return $this->redirectToRoute('app_contrat_commissionnaire');
             }
+        }
+        $formCr = $this->createForm(YearMonthType::class);
+        $formCr->handleRequest($request);
+        if ($formCr->isSubmitted() && $formCr->isValid()) {
+            $articles = $this->cc->findBy(['contratCommissionaire' => true]);
+            foreach ($articles as $value) {
+                if ($art == '') {
+                    $art = '\'' . $value->getReference() . '\'';
+                } else {
+                    $art = $art . ',' . '\'' . $value->getReference() . '\'';
+                }
+            }
+            $mois = $formCr->getData()['month'];
+            $annee = $formCr->getData()['year'];
+            $mouvs = $this->repoMouv->getVenteContratCommissionnaire($art, $mois, $annee);
+            //dd($mouvs);
         }
 
         return $this->render('contrat_commissionnaire/index.html.twig', [
@@ -78,7 +95,9 @@ class ContratCommissionnaireController extends AbstractController
             'title' => "contrats commissionnaires",
             'listeArticles' => $repo->findAll(),
             'listeMails' => $repoMails->findBy(['page' => $tracking]),
-            'form' => $form->createView()
+            'mouvs' => $mouvs,
+            'form' => $form->createView(),
+            'formCr' => $formCr->createView(),
         ]);
     }
 
@@ -88,11 +107,11 @@ class ContratCommissionnaireController extends AbstractController
     public function deleteMail($id, MailListRepository $repo): Response
     {
 
-       $search = $repo->findOneBy(['id' => $id]);
-        
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($search);
-            $em->flush();
+        $search = $repo->findOneBy(['id' => $id]);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($search);
+        $em->flush();
 
         $this->addFlash('message', 'le mail a bien été supprimé !');
         return $this->redirectToRoute('app_contrat_commissionnaire');
@@ -111,10 +130,10 @@ class ContratCommissionnaireController extends AbstractController
                 $article = new ProduitsCommissionnaires();
                 //dd('je suis passé');
                 $article->setReference($value['reference'])
-                        ->setDesignation($value['designation'])
-                        ->setContratCommissionaire(false)
-                        ->setUpdatedAt(new DateTime())
-                        ->setCreatedAt(new DateTime());
+                    ->setDesignation($value['designation'])
+                    ->setContratCommissionaire(false)
+                    ->setUpdatedAt(new DateTime())
+                    ->setCreatedAt(new DateTime());
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($article);
                 $em->flush();
@@ -130,15 +149,15 @@ class ContratCommissionnaireController extends AbstractController
      */
     public function updateArticle($id, ProduitsCommissionnairesRepository $cc): Response
     {
-            $article = $cc->findOneBy(['id' => $id ]);
-            if ($article->getContratCommissionaire() == false) {
-                $article->setContratCommissionaire(true);
-            }elseif ($article->getContratCommissionaire() == true) {
-                $article->setContratCommissionaire(false);
-            }
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($article);
-                $em->flush();
+        $article = $cc->findOneBy(['id' => $id]);
+        if ($article->getContratCommissionaire() == false) {
+            $article->setContratCommissionaire(true);
+        } elseif ($article->getContratCommissionaire() == true) {
+            $article->setContratCommissionaire(false);
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
 
         $this->addFlash('message', 'Mise à jour éffectuée avec succés !');
         return $this->redirectToRoute('app_contrat_commissionnaire');
@@ -149,40 +168,39 @@ class ContratCommissionnaireController extends AbstractController
      */
     public function sendMail(): Response
     {
-            
-            $articles = $this->cc->findBy(['contratCommissionaire' => true ]);
-            $art = '';
-            $d = date("Y-m-d");
-            $mois = date("m", strtotime($d."- 1 months"));
-            $annee = date("Y", strtotime($d."- 1 months"));
-            foreach ($articles as $value) {
-                if ($art == '') {
-                    $art = '\'' . $value->getReference() . '\'';
-                }else {
-                    $art = $art . ',' . '\'' . $value->getReference() . '\'';
-                }
+
+        $articles = $this->cc->findBy(['contratCommissionaire' => true]);
+        $art = '';
+        $d = date("Y-m-d");
+        $mois = date("m", strtotime($d . "- 1 months"));
+        $annee = date("Y", strtotime($d . "- 1 months"));
+        foreach ($articles as $value) {
+            if ($art == '') {
+                $art = '\'' . $value->getReference() . '\'';
+            } else {
+                $art = $art . ',' . '\'' . $value->getReference() . '\'';
             }
-            if (!is_null($art) | !empty($art)) {
-                $mouvs = $this->repoMouv->getVenteContratCommissionnaire($art, $mois, $annee);
-                $mailsList = $this->repoMail->findBy(['page' => 'app_contrat_commissionnaire']);
-                $mails = [];
-                foreach ($mailsList as $value) {
-                    array_push($mails, new Address( $value->getEmail() ) );
-                }
-                if (!empty($mails) | !is_null($mails) | !$mails == '') {
-                    // envoyer un mail
-                    $html = $this->renderView('mails/mailContratCommissionnaire.html.twig', ['mouvs' => $mouvs, 'annee' => $annee, 'mois' => $mois ]);
-                    $email = (new Email())
+        }
+        if (!is_null($art) | !empty($art)) {
+            $mouvs = $this->repoMouv->getVenteContratCommissionnaire($art, $mois, $annee);
+            $mailsList = $this->repoMail->findBy(['page' => 'app_contrat_commissionnaire']);
+            $mails = [];
+            foreach ($mailsList as $value) {
+                array_push($mails, new Address($value->getEmail()));
+            }
+            if (!empty($mails) | !is_null($mails) | !$mails == '') {
+                // envoyer un mail
+                $html = $this->renderView('mails/mailContratCommissionnaire.html.twig', ['mouvs' => $mouvs, 'annee' => $annee, 'mois' => $mois]);
+                $email = (new Email())
                     ->from($this->mailEnvoi)
                     ->to(...$mails)
                     ->subject('Liste des ventes de produits sous contrat commissionnaire')
                     ->html($html);
-                    $this->mailer->send($email);
-                }
+                $this->mailer->send($email);
             }
+        }
 
         return $this->redirectToRoute('app_contrat_commissionnaire');
     }
-
 
 }
