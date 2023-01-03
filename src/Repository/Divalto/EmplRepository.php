5a8389cd68372e50b673d62a98d646917206ef1e
J.Pochet <jpochet@lhermitte.fr>
2023-01-03 17:02:22 +0100

    modification de la requete SQL pour qu'elle affiche les résultats correctement, ajout de la désignation produit

--- src/Repository/Divalto/EmplRepository.php
+++ src/Repository/Divalto/EmplRepository.php
@@ -25,18 +25,18 @@ class EmplRepository extends ServiceEntityRepository
     public function getBadPlaceProduct(): array
     {
         $conn = $this->getEntityManager()->getConnection();
-        $sql = "SELECT dos, ref, sref1, sref2, depot, empl, tiers, depotVtl, emplVtl, vtlNumber, numeroPiece, opVtl, typePiece, RTRIM(LTRIM(s.DEPOT)) AS depotStock, SUM(s.QTETJSENSTOCK) AS stock, RTRIM(LTRIM(s.EMPLACEMENT)) AS emplStock
-        FROM(
-        SELECT RTRIM(LTRIM(dos)) as dos, RTRIM(LTRIM(ref)) AS ref, RTRIM(LTRIM(sref1)) AS sref1, RTRIM(LTRIM(sref2)) AS sref2, RTRIM(LTRIM(depot)) AS depot, RTRIM(LTRIM(empl)) AS empl,
-        RTRIM(LTRIM(v.TIERS)) AS tiers, RTRIM(LTRIM(v.DEPO)) AS depotVtl, RTRIM(LTRIM(v.LIEU)) AS emplVtl, RTRIM(LTRIM(v.VTLNO)) AS vtlNumber, RTRIM(LTRIM(v.PINO)) AS numeroPiece, RTRIM(LTRIM(v.OP)) AS opVtl, RTRIM(LTRIM(v.PICOD)) AS typePiece
+        $sql = "SELECT RTRIM(LTRIM(dos)) as dos, RTRIM(LTRIM(ref)) AS ref, RTRIM(LTRIM(sref1)) AS sref1, RTRIM(LTRIM(sref2)) AS sref2, RTRIM(LTRIM(a.DES)) AS designation, RTRIM(LTRIM(depot)) AS depot, RTRIM(LTRIM(empl)) AS empl,
+        RTRIM(LTRIM(v.TIERS)) AS tiers, RTRIM(LTRIM(v.DEPO)) AS depotVtl, RTRIM(LTRIM(v.LIEU)) AS emplVtl, RTRIM(LTRIM(v.VTLNO)) AS vtlNumber, RTRIM(LTRIM(v.PINO)) AS numeroPiece,
+		RTRIM(LTRIM(v.OP)) AS opVtl, RTRIM(LTRIM(v.PICOD)) AS typePiece, RTRIM(LTRIM(s.DEPOT)) AS depotStock, SUM(s.QTETJSENSTOCK) AS stock, RTRIM(LTRIM(s.EMPLACEMENT)) AS emplStock
         FROM(
         SELECT e.DOS AS dos, e.REF AS ref, e.SREF1 AS sref1, e.SREF2 AS sref2, e.DEPO AS depot, e.LIEU AS empl
         FROM ARTDEPO e
         WHERE e.DOS NOT IN (777)) reponse
         RIGHT JOIN MVTL v ON v.DOS = dos AND ref = v.REF AND sref1 = v.SREF1 AND sref2 = v.SREF2
-        WHERE v.PICOD IN (2,3) AND v.TICOD NOT IN('I') AND v.LIEU <> empl)rep
-        LEFT JOIN MVTL_STOCK_V s ON dos = s.DOSSIER AND ref = s.REFERENCE AND sref1 = s.SREFERENCE1 AND sref2 = s.SREFERENCE2 AND empl <> s.EMPLACEMENT
-        GROUP BY dos, ref, sref1, sref2, depot, empl, tiers, depotVtl, emplVtl, vtlNumber, numeroPiece, opVtl, typePiece, s.DEPOT, s.EMPLACEMENT
+        LEFT JOIN MVTL_STOCK_V s ON dos = s.DOSSIER AND ref = s.REFERENCE AND sref1 = s.SREFERENCE1 AND sref2 = s.SREFERENCE2
+		INNER JOIN ART a ON a.DOS = dos AND a.REF = ref
+		WHERE v.PICOD IN (2,3) AND v.TICOD NOT IN('I') AND (v.LIEU <> empl OR empl <> s.EMPLACEMENT)
+        GROUP BY dos, ref, sref1, sref2, a.DES, depot, empl, v.TIERS, v.DEPO, v.LIEU, v.VTLNO, v.PINO, v.OP, v.PICOD, s.DEPOT, s.EMPLACEMENT
         ORDER BY ref
         ";
         $stmt = $conn->prepare($sql);
